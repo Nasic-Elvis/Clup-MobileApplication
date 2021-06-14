22806490cc0c028d140057daede4ba3e2026078b
Nasic-Elvis <e.nasic@campus.unimib.it>
2021-06-14 22:25:41 +0200

    added icons and booking.dart page

---
+++ clup/lib/view/pages/booking/booking.dart
@@ -0,0 +1,41 @@
+import 'package:clup/model/booking.dart';
+import 'package:clup/model/store.dart';
+import 'package:clup/view/pages/booking/components/dateTimePicker.dart';
+import 'package:clup/view/pages/details/components/app_bar.dart';
+import 'package:clup/view/pages/details/components/body.dart';
+import 'package:flutter/material.dart';
+
+
+class BookingPage extends StatefulWidget {
+  static String routeName = "/booking";
+
+  @override
+  _BookingPage createState() => _BookingPage();
+}
+
+class _BookingPage extends State<BookingPage>
+    with TickerProviderStateMixin {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    final BookingDetailsArguments agrs =
+        ModalRoute.of(context).settings.arguments;
+    print(agrs.booking.day);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(rating: agrs.store.rating),
+      body: DateTimePicker(agrs.store),
+    );
+  }
+}
+
+class BookingDetailsArguments {
+  final Booking booking;
+  final Store store;
+  BookingDetailsArguments({@required this.booking, this.store});
+}
